27f8c3acf1e1c21605345ecf5b95bf545f1e54b5
Abhishek <aahabhisheksingh@gmail.com>
2023-04-12 22:08:43 +0530

    setting up the firebase

---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants{
+  static String appid="1:860316168979:web:680bf8052ca467d73460e6";
+  static String apikey="AIzaSyCIO4h_5uxVMYlLnLcL3OtMa0r1x1t5Ec0";
+  static String messageingSenderId="860316168979";
+  static String projectId="chating-and-data-sharing-app";
+}
